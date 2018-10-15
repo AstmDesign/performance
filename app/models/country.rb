852bf8b379325a581219a7344451654b0843bdaa
Astm <astm@Astm-MacBook.local>
2018-10-15 20:23:47 +0200

    define the DB relations

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,6 @@
 class Country < ActiveRecord::Base
+
+  # Define the DB relations
+  has_many :users
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
+
+  # Define the DB relations
   belongs_to :country
+
 end
