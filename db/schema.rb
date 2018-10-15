e03a4c7298b484948516ff02ad543666869c027a
Astm <astm@Astm-MacBook.local>
2018-10-15 10:58:22 +0200

    created the DB migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20181015085455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "countries", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "gender"
+    t.integer  "country_id"
+    t.boolean  "is_blockd"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "users", ["country_id"], name: "index_users_on_country_id", using: :btree
+
+  add_foreign_key "users", "countries"
 end
